632b5ed7b197fed4464a9403ace96ab2810568e1
dhrumildas <dhrumildas1@gmail.com>
2026-03-10 15:09:14 +0000

    added panel for dialogue

--- Assets/Scripts/Gameplay/Tutorial.cs
+++ Assets/Scripts/Gameplay/Tutorial.cs
@@ -1,29 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using MailSorting.Data;
 using MailSorting.Gameplay;
 
-public enum TutorialState
-{
-    IntroDialogue,
-    AcceptTask,
-    ReplyTask,
-    RejectTask,
-    ReportTask,
-    OutroDialogue
-}
-
-public class Tutorial : MonoBehaviour
+public class TutorialManager : MonoBehaviour
 {
     [Header("State Tracking")]
-    public TutorialState currentState = TutorialState.IntroDialogue;
+    public int stage = 0;
+    private bool waitingForButton = false;
+    private string buttonPressed = "";
 
     [Header("UI References")]
     public GameObject dialoguePanel;
     public TextMeshProUGUI agentText;
-    public GameObject spacebarPrompt; // "Press [SPACE] to continue"
+    public GameObject spacebarPrompt;
 
     [Header("Hardcoded Mails")]
     public Mail_Items_SO acceptMail;
@@ -35,103 +25,151 @@ public class Tutorial : MonoBehaviour
     public Transform letterSpawnPoint;
     public GameObject letterPrefab;
 
-    // Internal tracking
     private GameObject currentSpawnedMail;
-    private int dialogueIndex = 0;
-    private string[] currentDialogueArray;
-    private bool waitingForPlayerAction = false;
-
-    // --- HARDCODED DIALOGUE ---
-    private string[] introLines = {
-        "Welcome to A.V.A. Sorting Facility.",
-        "I am your supervising Agent. I will walk you through the basics.",
-        "Your job is to process mail based on daily directives.",
-        "Let's start simple. The rule today is: All mail must have a valid signature.",
-        "I'm sending you a perfectly fine letter. Approve it."
-    };
-    private string[] failLines = {
-        "Incorrect. Read the directives carefully.",
-        "Let's try that again. Here is a fresh copy."
-    };
 
     void Start()
     {
-        // Kick off the tutorial
-        StartDialogue(introLines);
+        dialoguePanel.SetActive(true);
+        LoadStage(0);
     }
 
     void Update()
     {
-        // Handle Dialogue Progression
-        if (!waitingForPlayerAction && dialoguePanel.activeSelf && Input.GetKeyDown(KeyCode.Space))
+        // Equivalent to your Check == "Space"
+        if (!waitingForButton && Input.GetKeyDown(KeyCode.Space))
         {
-            AdvanceDialogue();
+            LoadStage(stage + 1);
         }
     }
 
-    // --- DIALOGUE SYSTEM ---
-
-    private void StartDialogue(string[] lines)
+    public void LoadStage(int newStage)
     {
-        currentDialogueArray = lines;
-        dialogueIndex = 0;
-        dialoguePanel.SetActive(true);
+        stage = newStage;
+        waitingForButton = false;
         spacebarPrompt.SetActive(true);
-        agentText.text = currentDialogueArray[dialogueIndex];
-    }
 
-    private void AdvanceDialogue()
-    {
-        dialogueIndex++;
-
-        if (dialogueIndex < currentDialogueArray.Length)
+        switch (stage)
         {
-            agentText.text = currentDialogueArray[dialogueIndex];
-        }
-        else
-        {
-            // Dialogue finished! Trigger the next action
-            dialoguePanel.SetActive(false);
-            TriggerNextPhase();
-        }
-    }
-
-    private void TriggerNextPhase()
-    {
-        waitingForPlayerAction = true;
-
-        switch (currentState)
-        {
-            case TutorialState.IntroDialogue:
-                currentState = TutorialState.AcceptTask;
+            case 0:
+                agentText.text = "Welcome to Fan Mail Associate Service, yadda yadda yadda...";
+                break;
+            case 1:
+                agentText.text = "You're here to organise and sort mail and escalate it to the correct department.";
+                break;
+            case 2:
+                agentText.text = "I'm assuming you've done this before, anyway.";
+                break;
+            case 3:
+                agentText.text = "While you passed our checks, you still have a seven day probation to pass before we can sign you off. I'm putting a lot of trust in you for this.";
+                break;
+            case 4:
+                agentText.text = "So, seems here that I have to walk you through how to do your job...";
+                break;
+            case 5:
+                agentText.text = "In front of you is a letter, take a look at what it says.";
                 SpawnTutorialMail(acceptMail);
                 break;
-            case TutorialState.AcceptTask:
-                currentState = TutorialState.ReplyTask;
+            case 6:
+                agentText.text = "Now if you pay attention to the rules on the right, you can compare these to the letter to see if it passes.";
+                break;
+            case 7:
+                agentText.text = "To your left, there are buttons on the side of your desk. You can press the number keys 1, 2, 3 or 4 to quickly select these.";
+                break;
+            case 8:
+                agentText.text = "If all the rules are followed, the mail can be ACCEPTED.";
+                break;
+            case 9:
+                agentText.text = "If one rule was broken, the mail can be sent to another department to be REPLIED to.";
+                break;
+            case 10:
+                agentText.text = "If more than one rule is broken, the mail should be REJECTED.";
+                break;
+            case 11:
+                agentText.text = "If anything in the mail is suspicious or dangerous, you should REPORT it.";
+                break;
+            case 12:
+                agentText.text = "Okay, got all that? Go ahead and make a decision for the letter you have there.";
+                WaitForButton();
+                break;
+
+            // --- ACCEPT LETTER GRADING ---
+            case 13: // Accept Fail
+                agentText.text = $"Really? You thought that was {buttonPressed}? Try again, here.";
+                break;
+            case 14: // Accept Acceptable
+                agentText.text = "Okay, I see how you would think that, but that was actually an ACCEPT. Here's another letter, try again.";
+                break;
+            case 15: // Accept Correct
+                agentText.text = "Good, now how would you process this one?";
                 SpawnTutorialMail(replyMail);
+                WaitForButton();
                 break;
-            case TutorialState.ReplyTask:
-                currentState = TutorialState.RejectTask;
+
+            // --- REPLY LETTER GRADING ---
+            case 16: // Reply Fail
+                agentText.text = $"Really? You thought that was {buttonPressed}? Try again, here.";
+                break;
+            case 17: // Reply Acceptable
+                agentText.text = "Okay, I see how you would think that, but that was actually a REPLY. Here's another letter, try again.";
+                break;
+            case 18: // Reply Correct
+                agentText.text = "That's correct, you're getting the hang of this. Here's another letter.";
                 SpawnTutorialMail(rejectMail);
+                WaitForButton();
                 break;
-            case TutorialState.RejectTask:
-                currentState = TutorialState.ReportTask;
+
+            // --- REJECT LETTER GRADING ---
+            case 19: // Reject Fail
+                agentText.text = $"Really? You thought that was {buttonPressed}? Try again, here.";
+                break;
+            case 20: // Reject Acceptable
+                agentText.text = "Okay, I see how you would think that, but that was actually a REJECT. Here's another letter, try again.";
+                break;
+            case 21: // Reject Correct
+                agentText.text = "You're on a roll, okay, one last letter. Take a look at this.";
                 SpawnTutorialMail(reportMail);
+                WaitForButton();
                 break;
-            case TutorialState.ReportTask:
-                currentState = TutorialState.OutroDialogue;
-                waitingForPlayerAction = false;
-                StartDialogue(new string[] { "Good. You understand the basics.", "Clock in when you are ready to begin your real shift." });
+
+            // --- REPORT LETTER GRADING ---
+            case 22: // Report Fail
+                agentText.text = $"Really? You thought that was {buttonPressed}? Try again, here.";
                 break;
-            case TutorialState.OutroDialogue:
-                // TUTORIAL COMPLETE!
-                Debug.Log("Tutorial Finished! Time to load the Clock-In Scene.");
+            case 23: // Report Acceptable
+                agentText.text = "Okay, I see how you would think that, but that was actually a REPORT. Here's another letter, try again.";
+                break;
+            case 24: // Report Correct (Proceeds to Outro)
+                agentText.text = "Yeah, sorry you had to see that one, but you're gonna have to get used to it if you're gonna work here.";
+                break;
+
+            // --- OUTRO ---
+            case 25:
+                agentText.text = "Good. Well I'm too busy to answer questions, so you'll just have to get on with it. Tomorrow we have a bigger shipment of mail, so be prepared.";
+                break;
+            case 26:
+                Debug.Log("Tutorial Complete! Transition to Clock In Scene!");
                 // UnityEngine.SceneManagement.SceneManager.LoadScene("ClockInScene");
                 break;
+
+            case -1:
+                agentText.text = "I don't have time for this. George, you need to find me another mail sorter.";
+                waitingForButton = true; // Lock the game
+                spacebarPrompt.SetActive(false);
+                break;
         }
+
+        // Logic to "Rewind" if the player hit spacebar after failing a letter
+        if (stage == 13 || stage == 14) stage = 11; // Rewinds to "Make a decision..." (Accept)
+        if (stage == 16 || stage == 17) stage = 14; // Rewinds to waiting for Reply
+        if (stage == 19 || stage == 20) stage = 17; // Rewinds to waiting for Reject
+        if (stage == 22 || stage == 23) stage = 20; // Rewinds to waiting for Report
     }
 
-    // --- MAIL HANDLING ---
+    private void WaitForButton()
+    {
+        waitingForButton = true;
+        spacebarPrompt.SetActive(false);
+    }
 
     private void SpawnTutorialMail(Mail_Items_SO mailData)
     {
@@ -139,67 +177,37 @@ public class Tutorial : MonoBehaviour
 
         currentSpawnedMail = Instantiate(letterPrefab, letterSpawnPoint.position, Quaternion.identity, letterSpawnPoint);
 
-        // Setup the physical envelope
         EnvelopeObject envelope = currentSpawnedMail.GetComponent<EnvelopeObject>();
         if (envelope != null) envelope.Initialise(mailData);
 
         UI_DragCheck drag = currentSpawnedMail.GetComponent<UI_DragCheck>();
         if (drag != null) drag.mailData = mailData;
-
-        // Tell the Agent UI to give them a hint
-        dialoguePanel.SetActive(true);
-        spacebarPrompt.SetActive(false); // Hide spacebar prompt since they need to click a button now
-
-        switch (currentState)
-        {
-            case TutorialState.AcceptTask: agentText.text = "Click ACCEPT."; break;
-            case TutorialState.ReplyTask: agentText.text = "This one is missing a signature. Click REPLY."; break;
-            case TutorialState.RejectTask: agentText.text = "This is too heavy. Click REJECT."; break;
-            case TutorialState.ReportTask: agentText.text = "This contains contraband! Click REPORT immediately."; break;
-        }
     }
 
-    // --- BUTTON INTERCEPT ---
-    // Your UI Buttons will call these methods instead of ActionButtonController!
+    // --- BUTTON INTERCEPT METHODS ---
+    public void ClickAccept() => CheckAnswer(MailAction.Accept);
+    public void ClickReply() => CheckAnswer(MailAction.Reply);
+    public void ClickReject() => CheckAnswer(MailAction.Reject);
+    public void ClickReport() => CheckAnswer(MailAction.Report);
 
-    public void ClickAccept() => CheckPlayerAction(MailAction.Accept);
-    public void ClickReply() => CheckPlayerAction(MailAction.Reply);
-    public void ClickReject() => CheckPlayerAction(MailAction.Reject);
-    public void ClickReport() => CheckPlayerAction(MailAction.Report);
-
-    private void CheckPlayerAction(MailAction playerAction)
+    private void CheckAnswer(MailAction playerAction)
     {
-        if (!waitingForPlayerAction) return;
+        if (!waitingForButton) return;
 
-        MailAction expectedAction = MailAction.Accept; // Default
+        buttonPressed = playerAction.ToString().ToUpper();
+        if (currentSpawnedMail != null) Destroy(currentSpawnedMail);
 
-        // Determine what the right answer was supposed to be
-        if (currentState == TutorialState.AcceptTask) expectedAction = MailAction.Accept;
-        if (currentState == TutorialState.ReplyTask) expectedAction = MailAction.Reply;
-        if (currentState == TutorialState.RejectTask) expectedAction = MailAction.Reject;
-        if (currentState == TutorialState.ReportTask) expectedAction = MailAction.Report;
+        // Grade the current state based on what letter they are on
+        if (stage == 12) GradeTask(playerAction, MailAction.Accept, MailAction.Reply, 15, 14, 13);
+        else if (stage == 15) GradeTask(playerAction, MailAction.Reply, MailAction.Reject, 18, 17, 16);
+        else if (stage == 18) GradeTask(playerAction, MailAction.Reject, MailAction.Report, 21, 20, 19);
+        else if (stage == 21) GradeTask(playerAction, MailAction.Report, MailAction.Reject, 24, 23, 22);
+    }
 
-        if (playerAction == expectedAction)
-        {
-            // SUCCESS
-            if (currentSpawnedMail != null) Destroy(currentSpawnedMail);
-            waitingForPlayerAction = false;
-            StartDialogue(new string[] { "Correct." });
-            // Hitting spacebar after this will trigger the next phase!
-        }
-        else
-        {
-            // FAIL
-            if (currentSpawnedMail != null) Destroy(currentSpawnedMail);
-            waitingForPlayerAction = false;
-
-            // Go back one state so when the dialogue finishes, it repeats the same task!
-            if (currentState == TutorialState.AcceptTask) currentState = TutorialState.IntroDialogue;
-            else if (currentState == TutorialState.ReplyTask) currentState = TutorialState.AcceptTask;
-            else if (currentState == TutorialState.RejectTask) currentState = TutorialState.ReplyTask;
-            else if (currentState == TutorialState.ReportTask) currentState = TutorialState.RejectTask;
-
-            StartDialogue(failLines);
-        }
+    private void GradeTask(MailAction player, MailAction expected, MailAction acceptable, int correctStage, int acceptableStage, int failStage)
+    {
+        if (player == expected) LoadStage(correctStage);
+        else if (player == acceptable) LoadStage(acceptableStage);
+        else LoadStage(failStage);
     }
 }
